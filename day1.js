1a37b46993582d60a7e39d6f9c788b407bb160b3
OpenShift guest <changc@mit.edu>
2013-06-10 14:59:48 -0400

    Calculator updates.  Still not working completely

--- day1.js
+++ day1.js
@@ -1,11 +1,65 @@
  /*
-        Calculate: evaluate the value of an arithmetic expression
-    */
-    function calculate(){
-        var input = $('#text1:first'); //get input field from DOM
-        var val = input.val(); //get contents/val of object, return string
-        var output = $('#text1_out:first');
-        output.text(val);
-        //output.html(val);
-        //console.log(val);
+    Calculate: evaluate the value of an arithmetic expression
+*/
+function calculate(text){
+    var pattern = /\d+|\+|\-|\*|\/|\(|\) /g; //indicates a "regular expression" pattern
+    var tokens = text.match(pattern); //returns array
+    //tokens = JSON.stringify(tokens); //JSON = type of interchangable data structure
+    console.log(tokens.shift());
+    try{
+        var value = evaluate(tokens);
+        if (tokens.length == 0)
+            throw "ill-formed expression";
+        return String(value);
+    }catch(err){
+        return err;
     }
+}
+
+function setup_calc(div){
+    var input = $('<input></input>', {type: "Text", size: 50});
+    var output = $('<div></div>');
+    var button = $('<button>Calculate</button>');
+    button.bind("click", function(){
+        output.text(String(calculate(input.val())));
+    });
+    
+    $(div).append(input, button, output);
+}
+
+function read_operand(tokenArray){
+    var token = tokenArray.shift();
+    var num = parseInt(token);
+    if(isNaN(num))
+        throw "number expected";
+    return num;
+}
+
+function evaluate(tokenArray){
+    if(tokenArray.length == 0)
+        throw "missing operand";
+    var value = read_operand(tokenArray);
+    while(tokenArray.length !== 0){
+        var operator = tokenArray.shift();
+        if(['+', '-', '*', '/'].indexOf(operator) < 0)
+            throw "unrecognized operator";
+        if(tokenArray.length == 0)
+            throw "missing operand";
+        var temp = tokenArray.shift(); //double check
+        if(operator == "+"){
+            value = value + temp;
+        }else if (operator == "-"){
+            value = value - temp;
+        }else if (operator == "*"){
+            value = value*temp;
+        }else{
+            value = value/temp;
+        }
+    }
+    return value;
+}
+$(document).ready(function(){
+   $('.calculator').each(function(){
+       setup_calc(this);
+   })
+});
