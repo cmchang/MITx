b77ecdb951f52bc4c6b2aa532bd0d99d0793d92a
OpenShift guest <changc@mit.edu>
2013-06-11 13:46:06 -0400

    Updating calculator

--- apple-calculator.js
+++ apple-calculator.js
@@ -1,5 +1,3 @@
 function setup_calc(div){
-    var screen = $('<input></input>', {type: "Text", size: 50});
-    var button1 = $("<button>MC</button>");
-    $(div).append(screen, button1);
-}
+   \
+});
