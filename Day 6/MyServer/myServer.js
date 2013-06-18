8d2b7151740757e0bbebe695043add11f3ada7db
cmchang <changc@mit.edu>
2013-06-18 10:23:51 -0400

    current server updates

--- Day 6/MyServer/myServer.js
+++ Day 6/MyServer/myServer.js
@@ -1,7 +1,12 @@
 var sys = require("sys"),  
 my_http = require("http");  
-my_http.createServer(function(request,response){  
-    sys.puts("I got kicked");  //terminal prompt
+url = require("url"); //adds URL parser library
+
+my_http.createServer(function(request,response){
+    var URLstring = request.url;
+    console.log(request.url);
+    testParse(URLstring);
+    sys.puts("Server visited.");  //terminal prompt
     response.writeHeader(200, {"Content-Type": "text/plain",
                               "Access-Control-Allow-Origin": '*'
                               });  
@@ -9,3 +14,8 @@ my_http.createServer(function(request,response){
     response.end();  
 }).listen(8080);  
 sys.puts("Server Running on 8080");
+
+function testParse(urlStr){
+    var test = url.parse(urlStr, true ,true);
+    console.log(test);
+}
